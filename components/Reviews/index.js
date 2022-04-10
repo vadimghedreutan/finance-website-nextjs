1241d39809abb688dfc152d3fded9e72b22dd066
vadimghedreutan <vadim.ghedreutan@icloud.com>
2022-04-10 07:11:57 +0200

    updating 0.9

--- components/Reviews/index.js
+++ components/Reviews/index.js
@@ -1,10 +1,20 @@
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+
 function Reviews() {
   return (
     <section>
       <div className="container">
         <div className="reviews">
-          <div className="reviews_title">
-            <h1>People Say About Us</h1>
+          <div className="flex justify-center mb-7">
+            <div className="md:w-1/2 text-center">
+              <h1 className="text-[1.5rem] font-semibold">
+                Nice words by nicer people
+              </h1>
+              <span className=" text-gray-600 hidden md:inline-flex">
+                We work closely with our clients to help explore, refine and
+                execute their ideas. Here’s what some of them are saying...
+              </span>
+            </div>
           </div>
           <ul className="reviews_cards">
             <li>
@@ -29,6 +39,19 @@ function Reviews() {
               <cite>- Jane Doe</cite>
             </li>
           </ul>
+
+          <div className="flex justify-center mt-6">
+            <div className="flex items-center flex-row gap-2">
+              <div className="">
+                <BsFillArrowRightCircleFill className="w-8 h-8" />
+              </div>
+              <div>
+                <a href="#" className=" font-medium">
+                  Read more kind words
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- components/Team/index.js
+++ components/Team/index.js
@@ -16,12 +16,15 @@ function Team({ home }) {
 
           <div className="flex items-center justify-center text-center md:text-left p-2">
             <div>
-              <p className="text-lg text-white pb-2">
-                Our goal is to be at the heart of the financial services
-                industry as businesses expand across.
+              <span className="text-xl font-medium text-gray-700">
+                Hi there! We’re Company.
+              </span>
+              <p className="text-lg text-white pb-4 pt-1">
+                We’re an award-winning digital agency for bright ideas — a small
+                team of designers, developers and thinkers who are excited to
+                help you bring your idea to life. Here’s what we’re all about...
               </p>
-              <h3 className="font-semibold">Nelu Tofan</h3>
-              {/* <span className="text-xs text-gray-500"></span> */}
+              <h3 className="font-semibold text-gray-700">- Nelu Tofan</h3>
             </div>
           </div>
         </div>
