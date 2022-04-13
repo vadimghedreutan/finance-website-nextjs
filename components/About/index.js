187a5c8c15e7f35b2f4724fa0a8bcb4d14b6d028
vadimghedreutan <vadim.ghedreutan@icloud.com>
2022-04-13 17:40:33 +0200

    updating 1.4

--- components/About/index.js
+++ components/About/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import CheckBg from "../../images/check.svg";
 import { RichText } from "prismic-reactjs";
+import htmlSerializer from "../../utils/htmlSerializer";
 
 function About({ home }) {
   return (
@@ -9,7 +10,10 @@ function About({ home }) {
         <div className="about">
           <div className="about_content">
             <h1>{RichText.asText(home.about_title)}</h1>
-            {RichText.asText(home.about_description)}
+            <RichText
+              render={home.about_description}
+              htmlSerializer={htmlSerializer}
+            />
           </div>
           <div className="about_img">
             <Image src={CheckBg} />
