51f673e9400ed3cf1776571c75532c57a64c1bd4
vadimghedreutan <vadim.ghedreutan@icloud.com>
2022-05-12 18:37:45 +0200

    updating subscribe page

--- pages/success.js
+++ pages/success.js
@@ -1,33 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
 import InsuranceCards from "../components/InsuranceCards";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Seo from "../components/Seo";
+import Contact from "../components/Contact";
 
 function Success() {
   return (
-    <>
-      <section className="grid place-content-center">
-        <div className="container">
-          <div className="success">
-            <div className="success_title">
-              <h1>Mulțumim!</h1>
-              <p>
-                Cererea dumneavoastră a fost trimisă. În scurt timp echipa
-                noastră vă va contacta pentru mai multe detalii. Pentru a afla
-                cele mai noi informații legate de viața și integrarea în
-                Germania nu uita să te abonezi aici!
-              </p>
-              <Link href="/abonare" passHref>
-                <button className="btn_primery">Abonare</button>
-              </Link>
-            </div>
-            <div className="success_img">
-              <Image src="/svg/paper-plane.svg" width="600" height="400" />
+    <div className="wrapper">
+      <Seo
+        title="TofanConsult | Abonare"
+        description="Tofan Consult - Având libertatea de a lucra cu peste 350 de companii financiare din Germania, biroul nostru de consultanță te poate ajuta pe tine să găsești asigurările, creditele și produsele financiare de care ai cu adevărat nevoie."
+      />
+      <Header />
+      <main>
+        <section className="grid place-content-center">
+          <div className="container">
+            <div className="success">
+              <div className="success_title">
+                <h1>Mulțumim!</h1>
+                <p>
+                  Cererea dumneavoastră a fost trimisă. În scurt timp echipa
+                  noastră vă va contacta pentru mai multe detalii. Pentru a afla
+                  cele mai noi informații legate de viața și integrarea în
+                  Germania nu uita să te abonezi aici!
+                </p>
+                <Link href="/abonare" passHref>
+                  <button className="btn_primery">Abonare</button>
+                </Link>
+              </div>
+              <div className="success_img">
+                <Image src="/svg/paper-plane.svg" width="600" height="400" />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <InsuranceCards />
-    </>
+        </section>
+        <InsuranceCards />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
